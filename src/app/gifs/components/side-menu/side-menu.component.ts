75b7d5ca3df6b7b1f44fb4ff623fcb402eb84851
Uriel Solis <euss99@hotmail.com>
2025-06-29 13:02:14 -0600

    Agregar componentes de menú lateral: se crean los componentes 'side-menu-header' y 'side-menu-options' junto con sus respectivas plantillas HTML. Se actualizan las importaciones en 'side-menu.component.ts' para reflejar la nueva estructura de archivos.

--- src/app/gifs/components/side-menu/side-menu.component.ts
+++ src/app/gifs/components/side-menu/side-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-import { SideMenuHeader } from '../side-menu-header/side-menu-header.component';
-import { SideMenuOptions } from '../side-menu-options/side-menu-options.component';
+import { SideMenuHeader } from './side-menu-header/side-menu-header.component';
+import { SideMenuOptions } from './side-menu-options/side-menu-options.component';
 
 @Component({
   selector: 'side-menu',
